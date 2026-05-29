d36df5f9590ac61bb9fd39357786a116626dfc52
柯皓彬 <khb123456@aliyun.com>
2026-05-29 23:50:22 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 检查是否在虚拟环境中
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    # 检查虚拟环境是否存在
+    if [[ ! -d "venv" ]]; then
+        echo "创建虚拟环境..."
+        python3 -m venv venv
+    fi
+    
+    echo "激活虚拟环境..."
+    source venv/bin/activate
+fi
+pip install flask opencv-python pillow requests numpy
+python ai.py
